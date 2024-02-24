b34189ff5a0b42e31e027bbc4c98326977a3c2c4
lorenzonebolosi <lorenzo.nebolosi@mail.polimi.it>
2024-02-24 02:05:56 +0100

    Changed some things for making order

--- hydragnn/utils/thesis_work/Dataset.py
+++ hydragnn/utils/thesis_work/Dataset.py
@@ -15,36 +15,26 @@ from hydragnn.utils.thesis_work.parallel_read import parallel_processing
 class GraphDataset(AbstractBaseDataset):
 
     def __init__(self):
-        #time.sleep(2000)
         # Always initialize for multi-rank training.
-
-        comm = MPI.COMM_WORLD
         size, rank = hydragnn.utils.setup_ddp()
 
-        values =[]
         # Read all the saved tensors
         self.tensordictionary = []
         self.graph_list = []  # Contains all the graphs created
         # Get the length of the result folder
-        input_path = "hydragnn/utils/thesis_work/tensors"
+        input_path = "/Users/lorenzonebolosi/Desktop/Tesi/FreeFem_code/results/"
         values = os.listdir(input_path)
+        values.remove('.DS_Store')
+        values.remove('mesh.msh')
         local_values = list(nsplit(values, size))[rank]
         print("Process "+str(rank)+" has data: "+str(len(local_values)))
         parallel_graphs = parallel_processing(local_values)
         # Each file has a tensor for every iteration. So each file represent a complete run of the FreeFem code.
         MPI.COMM_WORLD.Barrier()
 
-        sendbuf = parallel_graphs
-        print("Process "+str(rank)+" has produced: "+str(len(sendbuf))+" graphs")
-        # Collect local array sizes using the high-level mpi4py gather
-        # sendcounts = np.array(comm.gather(len(sendbuf), root = 0))
-        # if rank == 0:
-        #     print("sendcounts: {}, total: {}".format(sendcounts, sum(sendcounts)))
-        #     recvbuf = [None] * (sum(sendcounts)) #Create an empylist of sendcounts elements
-        # else:
-        #     recvbuf = None
-
+        print("Process "+str(rank)+" has produced: "+str(len(parallel_graphs))+" graphs")
         deg = gather_deg(parallel_graphs)
+
         ## pickle
         basedir = os.path.join(os.path.dirname(__file__), "dataset", "pickle")
         attrs = dict()
@@ -57,24 +47,20 @@ class GraphDataset(AbstractBaseDataset):
             attrs=attrs,
         )
 
+        #gathered_data = comm.gather(sendbuf, root=0)
+        # if rank == 0:
+        #     print("Gathered array of: " +str(len(gathered_data)))
+        #
+        #
+        # if(rank == 0):
+        #     # I now have a list of lists that i need to flatten
+        #     flattened_list = [item for sublist in gathered_data for item in sublist]
+        #
+        #     self.graph_list = flattened_list
+        #     print("Converted input tensors into " + str(len(self.graph_list)) + " graphs")
+        #
 
-        gathered_data = comm.gather(sendbuf, root = 0)
         MPI.COMM_WORLD.Barrier()
-
-        #comm.Gatherv(sendbuf=sendbuf, recvbuf=(recvbuf, sendcounts), root=0)
-
-        if rank == 0:
-            print("Gathered array of: " +str(len(gathered_data)))
-
-        #comm.Gatherv(parallel_graphs, root=0)
-
-        if(rank == 0):
-            # I now have a list of lists that i need to flatten
-            flattened_list = [item for sublist in gathered_data for item in sublist]
-
-            self.graph_list = flattened_list
-            print("Converted input tensors into " + str(len(self.graph_list)) + " graphs")
-
 
 
     def get(self, idx):
